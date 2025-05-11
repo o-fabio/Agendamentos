36eb0ef60e003271a868fd422e9bc47ba5e309a6
o-fabio <fabio.sobreira2@gmail.com>
2025-05-10 22:51:46 -0300

    feat: :sparkles: cadastro consulta

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,28 +1,35 @@
 <template>
-    <form class="manual-appointment-form" @submit.prevent="cadastrarAgendamento">
-      <h2>Cadastrar Agendamento Manual</h2>
-  
-      <input type="text" v-model="novoAgendamento.paciente" placeholder="Nome do Paciente" required />
-      <input type="text" v-model="novoAgendamento.doutor" placeholder="Nome do Doutor" required />
-      <input type="text" v-model="novoAgendamento.especialidade" placeholder="Especialidade" required />
-      
-      <input type="date" v-model="novoAgendamento.data" required />
-      <input type="time" v-model="novoAgendamento.horario" required />
-  
-      <select v-model="novoAgendamento.tipo">
-        <option disabled value="">Tipo de Consulta</option>
-        <option value="Primeira consulta">Primeira consulta</option>
-        <option value="Retorno">Retorno</option>
-      </select>
-  
-      <textarea v-model="novoAgendamento.observacoes" placeholder="Observações (opcional)" rows="2"></textarea>
-  
-      <button type="submit">Cadastrar</button>
-    </form>
-  </template>
+ <div class="container">
+  <form class="manual-appointment-form" @submit.prevent="cadastrarAgendamento">
+    <h2>Cadastrar Agendamento Manual</h2>
+
+    <input type="text" v-model="novoAgendamento.paciente" placeholder="Nome do Paciente" required />
+    <input type="text" v-model="novoAgendamento.doutor" placeholder="Nome do Doutor" required />
+    <input type="text" v-model="novoAgendamento.especialidade" placeholder="Especialidade" required />
+
+    <input type="date" v-model="novoAgendamento.data" required />
+    <input type="time" v-model="novoAgendamento.horario" required />
+
+    <select v-model="novoAgendamento.tipo">
+      <option disabled value="">Tipo de Consulta</option>
+      <option value="Primeira consulta">Primeira consulta</option>
+      <option value="Retorno">Retorno</option>
+    </select>
+
+    <textarea v-model="novoAgendamento.observacoes" placeholder="Observações (opcional)" rows="2"></textarea>
+
+    <button type="submit">Cadastrar</button>
+  </form>
+ </div>
+</template>
 
 <script setup lang="ts">
 import { ref } from 'vue';
+import { doc, setDoc } from "firebase/firestore";
+import { db } from "../firebase";
+import type { User } from 'firebase/auth';
+
+const props = defineProps<{ auth: User }>()
 
 const novoAgendamento = ref({
   paciente: '',
@@ -34,11 +41,13 @@ const novoAgendamento = ref({
   observacoes: ''
 });
 
-const cadastrarAgendamento = () => {
+const cadastrarAgendamento = async () => {
   // Aqui você pode adicionar a lógica para cadastrar o agendamento
   console.log('Novo agendamento:', novoAgendamento.value);
+  await setDoc(doc(db, "appointments", props.auth.uid), {
+    ...novoAgendamento.value,
+  });
 
-  
   // Limpar os campos após o cadastro
   novoAgendamento.value = {
     paciente: '',
@@ -49,10 +58,17 @@ const cadastrarAgendamento = () => {
     tipo: '',
     observacoes: ''
   };
+
+  alert('Agendamento cadastrado com sucesso!');
 };
 </script>
 
 <style scoped>
+.container {
+  border: 1px solid #ccc;
+  padding: 8px;
+}
+
 .manual-appointment-form {
   display: grid;
   gap: 12px;
@@ -60,10 +76,9 @@ const cadastrarAgendamento = () => {
   padding: 20px;
   background-color: #f4f4f4;
   border-radius: 10px;
-  max-width: 800px;
   position: sticky;
   top: 16px;
-  max-height: 400px;
+  margin: 0 auto;
 }
 
 .manual-appointment-form h2 {
